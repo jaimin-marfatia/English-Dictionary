0843fb02cf28c8d3a2b4bafba1a8e7954857c01b
jaimin-marfatia <marfatiajaimin@gmail.com>
2021-11-28 19:07:58 -0500

    Started with processing the json response

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 
-function showMeanings() {
-  alert("hello");
+function ShowMeanings() {
+
+  // take the input from textbox
+  const userInput = document.getElementById("txtInput").value;
+  
+  CallDictionaryApi(userInput);
+}
+
+function CallDictionaryApi(_userInput) {
+
+  const baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + _userInput;
+
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then (data => ProcessJson(data));
+}
+
+function ProcessJson(data) {
+
+  data.forEach(element => {
+    console.log(element.meanings);
+  });
+
+  // console.log(data);
 }
